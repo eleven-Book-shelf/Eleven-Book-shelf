103415715c50950b6862504a159f74fbb2e4686d
Han <nuher038@gmail.com>
2024-07-18 19:37:01 +0900

    init

---
+++ src/main/java/com/sparta/elevenbookshelf/ElevenBookShelfApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.elevenbookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevenBookShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevenBookShelfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/elevenbookshelf/ElevenBookShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.elevenbookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevenBookShelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
