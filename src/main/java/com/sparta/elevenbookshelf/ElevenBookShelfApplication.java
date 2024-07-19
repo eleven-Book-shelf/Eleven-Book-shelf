a3c775d6d4c2c852b452e5fd3ad84a9a39fa1e4c
2minus <blecker0610@gmail.com>
2024-07-19 23:17:53 +0900

    [#5] fix: 설정 파일 수정
    - application.properties : 변수명 jwt.key -> jwt.secret-key로 변경
    
    - Application.java : @EnableJpaAuditing 추가
    - JpaConfig.java : @EnableJpaAuditing 삭제, QueryDsl 관련 코드 추가
    - RestTemplateConfig.java : QueryDsl 관련 코드 삭제
    
    - ErrorCode.java : post, user 관련 사항 추가
    
    - SecurityConfig.java : api url users/ -> user/ 로 수정
    
    - JwtAuthenticationEntryPoint.java : objectMapper 역직렬화 오류 발생 -> JavaTimeMapper module 추가하는 코드 추가
    
    - Timestamp.java : @Temporal 추가

--- src/main/java/com/sparta/elevenbookshelf/ElevenBookShelfApplication.java
+++ src/main/java/com/sparta/elevenbookshelf/ElevenBookShelfApplication.java
@@ -2,10 +2,11 @@ package com.sparta.elevenbookshelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableAspectJAutoProxy
+
 @SpringBootApplication
+@EnableJpaAuditing
 public class ElevenBookShelfApplication {
 
     public static void main(String[] args) {
