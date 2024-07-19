a81106cd40b2549eb1ff0e20706ba2ba0bd51335
2minus <blecker0610@gmail.com>
2024-07-19 23:24:39 +0900

    [#5] fix: 설정 파일 수정
    - AuthController.java : LoginResponseDto로 토큰 body에도 반환하도록 수정
    - AuthService.java : LoginResponseDto 추가로 인한 수정
    - LoginResponseDto.java : @Builder 대신 생성자로 변경
    
    - Board.java : update method 추가
    - Post.java : User 연관관계 추가, 필드 추가로 인한 생성자 수정, update method 추가
    
    - User.java : post 연관관계 추가
    - UserController.java : 반환값 ? -> UserResponseDto
    
    - UserDetailsServiceImpl.java, UserService.java : Errorcode 수정으로 인한 수정

--- src/main/java/com/sparta/elevenbookshelf/dto/LoginResponseDto.java
+++ src/main/java/com/sparta/elevenbookshelf/dto/LoginResponseDto.java
@@ -4,10 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class LoginResponseDto {
 
     private String accessToken;
     private String refreshToken;
 
+    public LoginResponseDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
