74028a1687907c10dc04e40ed707e9d24c9e79bf
2minus <blecker0610@gmail.com>
2024-07-22 15:39:54 +0900

    [#5] fix : LoginResponseDto 빌더 패턴으로 수정

--- src/main/java/com/sparta/elevenbookshelf/service/AuthService.java
+++ src/main/java/com/sparta/elevenbookshelf/service/AuthService.java
@@ -41,7 +41,11 @@ public class AuthService {
         String refeshToken = jwtService.generateRefreshToken(user.getUsername());
 
         user.addRefreshToken(refeshToken);
-        return new LoginResponseDto(accessToken, refeshToken);
+
+        return LoginResponseDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refeshToken)
+                .build();
     }
 
     public void logout(User userid){
