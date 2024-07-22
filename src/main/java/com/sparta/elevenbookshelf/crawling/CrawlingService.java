84ba79d80860506444ffa46f19c37b7387673d23
Imnotcoderdude <topsolo7@naver.com>
2024-07-22 23:04:12 +0900

    [#2] feat : 동적 스크롤 적용. 추출 데이터 표본 추가.
    - 제목, 작가, 사이트, 타입, 별점 추출 가능.
    - 별점은 조회수와 같은 데이터를 공유하기에 설정 필요.

--- src/main/java/com/sparta/elevenbookshelf/crawling/CrawlingService.java
+++ src/main/java/com/sparta/elevenbookshelf/crawling/CrawlingService.java
@@ -10,10 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.HashSet;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +19,7 @@ import java.util.Set;
 public class CrawlingService {
 
     private final WebDriver webDriver;
+    private Set<String> disAllowedLink;
 
     @Value("${CRAWLING_PAGE}")
     private String kCrawlingPage;
@@ -33,11 +32,6 @@ public class CrawlingService {
 
     @Value("${CRAWLING_ART_LINK}")
     private String pageArtLink;
-
-    @Value("${COMICS_ART_TITLE}")
-    private String artTitle;
-
-    private Set<String> disAllowedLink;
 
     // robots.txt 파일에 규정된 접근 금지 목록.
     // TODO : 사이트별 robots.txt 규약을 적응형으로 적용하게끔 수정 필요.
@@ -59,25 +53,46 @@ public class CrawlingService {
         return false;
     }
 
-    // 로그인이 필요한 페이지인지 체크.
-    // TODO : 현재 동작 X. css 요소를 로드하는게 느리거나 요소를 아예 찾지 못하는 상황 발생중.
-    private boolean checkTheLoginPage() {
-        String pageSource = webDriver.getPageSource();
-        log.info("페이지 소스 : {}", pageSource);
-        if (pageSource.contains("로그인")) {
-            log.warn("로그인이 필요한 페이지임이 확인됨");
-            return true;
-        }
-        return false;
-    }
-
     // 페이지 로딩을 기다리는 메서드.
     private void waitForPage() {
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
         wait.until(webDriver
                 -> ((JavascriptExecutor) webDriver)
                 .executeScript("return document.readyState")
                 .equals("complete"));
+    }
+
+    // 동적페이지에서 스크롤을 끝까지 내려주는 메서드.
+    private void scrollToEndOfPage() {
+        // 자바 스크립트 코드를 실행 할 수 있도록 JavascriptExecutor 객체 생성.
+        JavascriptExecutor js = (JavascriptExecutor) webDriver;
+
+        // 현재 페이지의 총 높이를 저장.
+        long lastHeight = (long) js.executeScript("return document.body.scrollHeight");
+
+        // 스크롤이 바닥에 닿을때까지 반복
+        while (true) {
+            // 페이지를 끝까지 스크롤함.
+            js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+            try {
+                // 2초동안 쓰레드 중지로 페이지가 로드될 시간 벌기.
+                Thread.sleep(2000);
+
+            } catch (InterruptedException e) {
+                log.error("쓰레드 슬립 도중 에러 발생 : {}", e.getMessage());
+            }
+
+            // 스크롤을 최하단까지 내린후 페이지의 총 높이를 다시 저장.
+            long newHeight = (long) js.executeScript("return document.body.scrollHeight");
+
+            // 이번 반복에서 저장한 페이지의 총 높이와 이전 반복에서 저장한 페이지의 총 높이가 같다면 반복을 종료.
+            if (newHeight == lastHeight) {
+                break;
+            }
+
+            // 마지막 반복의 높이값을 계속 업데이트하여 조건문에서 검사가 가능하도록 함.
+            lastHeight = newHeight;
+        }
     }
 
     // 복수 : css 로딩을 기다리는 메서드.
@@ -92,6 +107,7 @@ public class CrawlingService {
         return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
+    // 크롤링 메서드
     public void performGoogleSearch() {
 
         log.info("크롤링 시작.");
@@ -99,79 +115,98 @@ public class CrawlingService {
         log.info("크롤링 할 페이지 : " + webDriver.getCurrentUrl());
 
         try {
-            while (true) {
-                waitForPage();
-                List<WebElement> linkElements = waitForElements(By.cssSelector(pageArtLink), 10);
-                log.info("찾은 링크 개수 {}: ", linkElements.size());
+            waitForPage();
+            scrollToEndOfPage();
 
-                //
-                if (linkElements.isEmpty()) {
-                    break;
+            List<WebElement> linkElements = waitForElements(By.cssSelector(pageArtLink), 10);
+            log.info("찾은 링크 개수 {}: ", linkElements.size());
+
+            if (linkElements.isEmpty()) {
+                log.error("링크가 없습니다.");
+                return;
+            }
+
+            // 모든 링크를 수집하여 별도의 리스트에 저장
+            List<String> allLinks = new ArrayList<>();
+            for (WebElement linkElement : linkElements) {
+                String artUrl = linkElement.getAttribute("href");
+                if (!checkTheLink(artUrl)) {
+                    log.info("저장된 작품 링크 : {}", artUrl);
+                    allLinks.add(artUrl);
                 }
+            }
 
-                for (int i = 0; i < linkElements.size(); i++) {
-                    try {
-                        // cssSelector 사용. 작품 링크 서치.
-                        // TODO : 현재 표적 사이트가 스크롤을 계속할시 생성되는 사이트라 스크롤 전까지만 링크를 찾아냄. 적응형 필요.
-                        linkElements = webDriver.findElements(By.cssSelector(pageArtLink));
-                        log.info("링크 목록의 크기 : {}", linkElements.size());
+            int totalLinks = allLinks.size();
+            for (int i = 0; i < totalLinks; i++) {
 
-                        // 인덱스가 요소보다 클경우
-                        if (i >= linkElements.size()) {
-                            log.warn("i의 크기가 링크 목록보다 큽니다 : {}", i);
-                            continue;
-                        }
+                String artUrl = allLinks.get(i);
+                log.info("현재 링크 위치 : {}/{}", i + 1, totalLinks);
 
-                        // href 로 정의되어 있는 작품 링크를 찾아서 들어감.
-                        String artUrl = linkElements.get(i).getAttribute("href");
-                        log.info("작품정보 URL : {}", artUrl);
+                try {
+                    log.info("작품정보 URL : {}", artUrl);
+                    waitForPage();
+                    webDriver.get(artUrl);
 
-                        // robots.txt 규약 위반시 이전 페이지로 이동.
-                        if (checkTheLink(artUrl)) {
-                            log.error("!!!robots.txt 규약 위반!!!! {}", artUrl);
-                            webDriver.navigate().back();
-                            continue;
-                        }
-
-                        webDriver.get(artUrl);
-                        log.info("찾은 링크로 이동 {}: ", artUrl);
-                        waitForPage();
-
-                        // 연령제한이 있는 페이지의 경우 로그인이 필요함. 로그인 페이지를 넘기기 위한 메서드.
-//                        if (checkTheLoginPage()) {
-//                            log.info("로그인이 필요한 페이지입니다. 링크 건너뛰고 다음으로 이동 : {}", artUrl);
-//                            webDriver.navigate().back();
-//                            wait.until(webDriver
-//                                    -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
-//                            continue;
-//                        }
-
-                        WebElement titleElement = waitForElement(By.cssSelector(artTitle), 10);
-                        String title = titleElement.getText();
-                        log.info("제목 찾기 : {}", title);
-
+                    // robots.txt 규약 위반시 이전 페이지로 이동.
+                    if (checkTheLink(artUrl)) {
+                        log.error("!!!robots.txt 규약 위반!!!! {}", artUrl);
                         webDriver.navigate().back();
-                        log.info("이전 페이지로 돌아갑니다.");
-                        waitForPage();
-
-                        // 하나의 데이터를 얻어온 후 2초간 쓰레드 정지. (표적 사이트 서버의 부하를 줄이기 위한 조치.)
-                        Thread.sleep(2000);
-
-                    } catch (NoSuchElementException e) {
-                        log.error("요소를 찾을 수 없습니다: {}", e.getMessage(), e);
-
-                    } catch (TimeoutException e) {
-                        // 페이지 리소스 파일 검사하여 로그인 페이지를 넘기는 메서드가 동작하지 않기에 타임아웃 발생 = 로그인 페이지 로 가정하고 다음 반복으로 넘어가게끔 설정.
-                        log.error("요소를 찾는 시간 초과: {}", e.getMessage(), e);
-                        webDriver.navigate().back();
-                        waitForPage();
-
-                    } catch (Exception e) {
-                        log.error("catch : 링크 처리중 오류 발생 : {}", e.getMessage(), e);
-                        webDriver.navigate().back();
-                        waitForPage();
-
+                        continue;
                     }
+
+                    waitForPage();
+                    webDriver.get(artUrl);
+                    log.info("찾은 링크로 이동 {}: ", artUrl);
+
+                    waitForPage();
+                    WebElement authorElement = waitForElement(By.xpath("//meta[@property='og:title']"), 10);
+                    String title = authorElement.getAttribute("content");
+                    log.info("작품 제목 : {}", title);
+
+                    waitForPage();
+                    WebElement titleElement = waitForElement(By.xpath("//meta[@name='author']"), 10);
+                    String author = titleElement.getAttribute("content");
+                    log.info("작가 : {}", author);
+
+                    waitForPage();
+                    WebElement siteElement = waitForElement(By.xpath("//meta[@property='og:site_name']"), 10);
+                    String site = siteElement.getAttribute("content");
+                    log.info("작품 게시 사이트 : {}", site);
+
+                    waitForPage();
+                    String type = webDriver.getTitle();
+                    if (type.contains("웹툰")) {
+                        log.info("작품 타입 : 웹툰");
+                    } else {
+                        log.info("작품 타입 : 웹소설");
+                    }
+
+                    waitForPage();
+                    WebElement element = waitForElement(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/a/div/div[1]/div[2]/span"), 10);
+                    String RaRaTing = element.getText();
+                    log.info("별점 : " + RaRaTing);
+
+                    webDriver.navigate().back();
+                    log.info("이전 페이지로 돌아갑니다.");
+                    waitForPage();
+
+                    // 하나의 데이터를 얻어온 후 2초간 쓰레드 정지. (표적 사이트 서버의 부하를 줄이기 위한 조치.)
+                    Thread.sleep(2000);
+
+                } catch (NoSuchElementException e) {
+                    log.error("요소를 찾을 수 없습니다: {}", e.getMessage(), e);
+                    log.error("페이지 소스 : {}", webDriver.getPageSource());
+
+                } catch (TimeoutException e) {
+                    // 페이지 리소스 파일 검사하여 로그인 페이지를 넘기는 메서드가 동작하지 않기에 타임아웃 발생 = 로그인 페이지 로 가정하고 다음 반복으로 넘어가게끔 설정.
+                    log.error("요소를 찾는 시간 초과: {}", e.getMessage(), e);
+                    webDriver.navigate().back();
+                    waitForPage();
+
+                } catch (Exception e) {
+                    log.error("catch : 링크 처리중 오류 발생 : {}", e.getMessage(), e);
+                    webDriver.navigate().back();
+                    waitForPage();
 
                 }
 
