f60791c675661d11a343eecd9d6a56baf2c2c369
Imnotcoderdude <topsolo7@naver.com>
2024-07-23 20:15:37 +0900

    [#2] fix : 코드 병합 에러 수정.

--- src/main/java/com/sparta/elevenbookshelf/security/SecurityConfig.java
+++ src/main/java/com/sparta/elevenbookshelf/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -85,13 +86,6 @@ public class SecurityConfig {
                                                    .requestMatchers("/login.html").permitAll()
                                                    .requestMatchers("/admin/**").hasRole("ADMIN")
                                                    .anyRequest().authenticated()
-        );
-
-        http.sessionManagement(sessionManagement -> sessionManagement
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                                                   .anyRequest().permitAll()
-
         );
 
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
