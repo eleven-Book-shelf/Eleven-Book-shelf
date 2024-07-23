1e0c9ea02260d493a5d19b5f225f700676f47109
minsik0 <qwew1212@naver.com>
2024-07-23 19:13:28 +0900

    ✨ update  대댓글 생성, 조회 기능추가

--- src/main/java/com/sparta/elevenbookshelf/entity/Comment.java
+++ src/main/java/com/sparta/elevenbookshelf/entity/Comment.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -25,15 +28,32 @@ public class Comment {
     @ManyToOne
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
 
     @Builder
-    public Comment(String contents, User user, Board board){
+    public Comment(String contents, User user, Board board, Comment parent){
         this.contents = contents;
         this.user = user;
         this.board = board;
+        this.parent = parent;
+        this.children = new ArrayList<>();
     }
 
     public void updateContent(String contents) {
         this.contents = contents;
     }
+
+    public void updateParent(Comment parent) {
+        this.parent = parent;
+    }
+
+    public void updateBoard(Board board) {
+        this.board = board;
+    }
+
 }
