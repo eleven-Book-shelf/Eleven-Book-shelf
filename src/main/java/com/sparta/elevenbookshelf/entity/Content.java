6fc9f272803f72c5c992a47cc2bc86159da16efc
2minus <blecker0610@gmail.com>
2024-07-25 10:12:45 +0900

    [#5] feat : content entity, repository 추가 버전

---
+++ src/main/java/com/sparta/elevenbookshelf/entity/Content.java
@@ -0,0 +1,58 @@
+package com.sparta.elevenbookshelf.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Content {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_id")
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String author;
+
+    private String platform;
+
+    private Double view;
+
+    private Double rating;
+
+    @Enumerated(EnumType.STRING)
+    private ContentType type;
+
+    @Enumerated(EnumType.STRING)
+    private ContentEnd isEnd;
+
+    @OneToMany(mappedBy = "content", cascade = CascadeType.MERGE)
+    private List<Post> posts = new ArrayList<>();
+
+//:::::::::::::::::// enum //::::::::::::::::://
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum ContentType {
+        COMICS,
+        NOVEL
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum ContentEnd {
+        END,
+        NOT
+    }
+}
---
+++ src/main/java/com/sparta/elevenbookshelf/repository/contentRepository/ContentRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.sparta.elevenbookshelf.repository.contentRepository;
+
+public interface ContentRepositoryCustom {
+}
