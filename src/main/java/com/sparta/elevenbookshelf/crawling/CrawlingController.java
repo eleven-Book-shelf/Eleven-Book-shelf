1270bdd829c39deec398a727cae15b964ebb97c9
Imnotcoderdude <topsolo7@naver.com>
2024-07-30 01:24:39 +0900

    [#25] ✨feat : 모든 크롤링 서비스 실행 엔드포인트 구현.

--- src/main/java/com/sparta/elevenbookshelf/crawling/CrawlingController.java
+++ src/main/java/com/sparta/elevenbookshelf/crawling/CrawlingController.java
@@ -3,10 +3,17 @@ package com.sparta.elevenbookshelf.crawling;
 import com.sparta.elevenbookshelf.crawling.Service.*;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/crawling")
 public class CrawlingController {
 
     private final MNovelService mNovelService;
@@ -15,20 +22,19 @@ public class CrawlingController {
     private final RNovelService rNovelService;
     private final CrawlingUtil crawlingUtil;
 
-    @PostConstruct
-    public void startCrawling() {
+    @PostMapping("/start/all")
+    public ResponseEntity<Void> allCrawlingStart() {
 
-//        mNovelService.mNovelsStart(); // 아직 Content 엔티티와 연결 안됨.
-//        kNovelService.kNovelsStart(); // 아직 Content 엔티티와 연결 안됨.
+        mNovelService.mNovelsStart();
         kComicsService.kComicsStart();
-//        rNovelService.rNovelsStart(); // 아직 Content 엔티티와 연결 안됨.
+        kNovelService.kNovelsStart();
+        rNovelService.rNovelsStart();
 
-        // CSV 파일 변환하여 로컬에 저장 기능
         crawlingUtil.exportToCsv();
 
-        // 로컬 CSV 파일 조회하여 DB 업데이트. 수정중.
-//        crawlingUtil.updateDatabase();
+        crawlingUtil.updateDatabase();
 
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
