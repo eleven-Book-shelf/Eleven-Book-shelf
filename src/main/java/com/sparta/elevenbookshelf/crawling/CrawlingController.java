c098ffe9c82b5c02bf91f1f1e86b7aeb48665de8
Imnotcoderdude <topsolo7@naver.com>
2024-07-30 20:36:58 +0900

    [#2] ✨feat : Content 단건 조회 추가.

--- src/main/java/com/sparta/elevenbookshelf/crawling/CrawlingController.java
+++ src/main/java/com/sparta/elevenbookshelf/crawling/CrawlingController.java
@@ -1,52 +1,58 @@
 package com.sparta.elevenbookshelf.crawling;
 
-import com.sparta.elevenbookshelf.crawling.Service.KPageService;
-import com.sparta.elevenbookshelf.crawling.Service.MNovelService;
-import com.sparta.elevenbookshelf.crawling.Service.RNovelService;
+import com.sparta.elevenbookshelf.crawling.Service.CrawlingService;
+import com.sparta.elevenbookshelf.dto.ContentDataResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/content")
 public class CrawlingController {
 
-    private final MNovelService mNovelService;
-    private final KPageService kPageService;
-    private final RNovelService rNovelService;
-    private final CrawlingUtil crawlingUtil;
+    private final CrawlingService crawlingService;
 
+    // 전체 크롤링 시작.
     @PostMapping("/start/all")
     public ResponseEntity<Void> allCrawlingStart() {
 
-        mNovelService.mNovelsStart();
-        kPageService.serviceStart();
-        rNovelService.rNovelsStart();
-
-        crawlingUtil.exportToCsv();
-
-        crawlingUtil.updateDatabase();
+        crawlingService.allCrawlingStart();
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    // 로컬 파일로 저장.
     @GetMapping("/local")
     public ResponseEntity<Void> csvFileToLocal() {
-        crawlingUtil.exportToCsv();
+
+        crawlingService.csvFileToLocal();
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    // 로컬 파일로 DB 업데이트.
     @PostMapping("/local/update")
     public ResponseEntity<Void> localToDataBase() {
-        crawlingUtil.updateDatabase();
+
+        crawlingService.localToDataBase();
 
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    // Content 단건 조회
+    @GetMapping
+    public ResponseEntity<ContentDataResponseDto> getOneContent(@RequestParam String contentUrl) {
+
+        String decodeUrl = URLDecoder.decode(contentUrl, StandardCharsets.UTF_8);
+
+        ContentDataResponseDto responseDto = crawlingService.getOneContent(decodeUrl);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
 }
